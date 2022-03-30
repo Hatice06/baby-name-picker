7f4b2fa2fc5f2eff6d94e07b2b10d3532829280f
Hatice06 <haticeaydogan00@gmail.com>
2022-03-30 23:08:16 +0100

    baby name picker level 1 works

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import "./App.css";
 import babyNamesData from "./babyNamesData.json";
 
 function App() {
+  const sortedNames =babyNamesData.sort((a,b)=> a.name > b.name ? 1 : -1);
   return (
     <div className="baby-name-container">
-      {babyNamesData.map((baby) => {
+      <p>
+        {sortedNames.map((baby) => {
         if (baby.sex === "f") {
           return <span className="baby-girl"> {baby.name} </span>;
         } else {
           return <span className="baby-boy"> {baby.name} </span>;
         }
       })}
+      </p>
+      <hr/>
+      
     </div>
   );
 }
