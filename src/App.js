4b4dc25425e1eab05c95f8ce0197861a1be1790e
Hatice06 <haticeaydogan00@gmail.com>
2022-04-21 23:04:29 +0100

    Level 2 completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import babyNamesData from "./babyNamesData.json";
+import AllBabyNames from "./AllBabyNames";
 
 function App() {
-  const sortedNames =babyNamesData.sort((a,b)=> a.name > b.name ? 1 : -1);
+
+
+  const[sortedNames, setSortedNames]=useState(babyNamesData.sort((a,b)=> a.name > b.name ? 1 : -1));
+
+  const handleClick = (event) => {
+    const searchName =babyNamesData.filter(item => item.name.includes(event.target.value))
+   setSortedNames(searchName);
+   console.log(searchName);
+  }
+
   return (
     <div className="baby-name-container">
-      <p>
-        {sortedNames.map((baby) => {
-          if (baby.sex === "f") {
-            return <span className="baby-girl"> {baby.name} </span>;
-          } else {
-            return <span className="baby-boy"> {baby.name} </span>;
-          }
-        })}
-      </p>
+      <input type="text" placeholder="Search baby name" onKeyUp={handleClick}> 
+      </input>
+      <AllBabyNames sortedNames={sortedNames} />
       <hr />
     </div>
   );
