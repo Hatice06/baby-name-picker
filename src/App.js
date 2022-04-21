cd3d3272b955f963dee20836f8de239d170719a4
Hatice06 <haticeaydogan00@gmail.com>
2022-04-21 23:14:28 +0100

    Case insensitive

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import "./App.css";
 import babyNamesData from "./babyNamesData.json";
 import AllBabyNames from "./AllBabyNames";
+import SearchBar from "./SearchBar";
 
 function App() {
 
@@ -9,15 +10,14 @@ function App() {
   const[sortedNames, setSortedNames]=useState(babyNamesData.sort((a,b)=> a.name > b.name ? 1 : -1));
 
   const handleClick = (event) => {
-    const searchName =babyNamesData.filter(item => item.name.includes(event.target.value))
+    const searchName =sortedNames.filter(item => item.name.toLowerCase().includes(event.target.value.toLowerCase()))
    setSortedNames(searchName);
    console.log(searchName);
   }
 
   return (
     <div className="baby-name-container">
-      <input type="text" placeholder="Search baby name" onKeyUp={handleClick}> 
-      </input>
+      <SearchBar handleClick={handleClick}/>
       <AllBabyNames sortedNames={sortedNames} />
       <hr />
     </div>
